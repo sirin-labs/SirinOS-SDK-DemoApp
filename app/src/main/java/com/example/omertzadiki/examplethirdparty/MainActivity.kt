6ea8cd7ce97a3185c5a10903691846d46c60b34a
omer Tzadiki <omertzadiki@omers-MacBook-Pro.local>
2018-08-29 11:20:13 +0300

    add buttons to each method

--- app/src/main/java/com/example/omertzadiki/examplethirdparty/MainActivity.kt
+++ app/src/main/java/com/example/omertzadiki/examplethirdparty/MainActivity.kt
@@ -4,24 +4,80 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 
 import com.example.omertzadiki.examplethirdparty.Constants.*
 import com.sirinlabs.walletconnectionsdk.WalletCommunicationManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+
+    private val COIN_TYPE: String = "ETH"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         WalletCommunicationManager.addFinishInitListener {
-            WalletCommunicationManager.sendTransaction(successMethod = {
-                Log.d("omer","got back to app with success flag")
-            }, failureMethod = {
-                Log.d("omer","got back to app with failure flag")
-            })
+            initButtons()
         }
 
         WalletCommunicationManager.init(applicationContext)
         setContentView(R.layout.activity_main)
+    }
+
+    fun initButtons() {
+        send_transaction_btn.setOnClickListener {
+            sendTransaction()
+        }
+
+        sign_transaction_btn.setOnClickListener {
+            signTransaction()
+        }
+
+        get_address_btn.setOnClickListener {
+            getAddress()
+        }
+
+        get_rpc_address_btn.setOnClickListener {
+            getRpcAddress()
+        }
+
+        get_chain_id_btn.setOnClickListener {
+            getChainId()
+        }
+    }
+
+    fun sendTransaction() {
+        WalletCommunicationManager.sendTransaction(successMethod = {
+            Log.d("omer","got back to app with success flag")
+        }, failureMethod = {
+            Log.d("omer","got back to app with failure flag")
+        })
+    }
+
+    fun signTransaction() {
+        WalletCommunicationManager.sendTransaction(successMethod = {
+            Log.d("omer","got back to app with success flag")
+        }, failureMethod = {
+            Log.d("omer","got back to app with failure flag")
+        })
+    }
+
+
+    fun getAddress() {
+        toastValue(WalletCommunicationManager.getPublicAddress(COIN_TYPE))
+    }
+
+    fun getChainId() {
+        toastValue(WalletCommunicationManager.getChainId(COIN_TYPE).toString())
+    }
+
+    fun getRpcAddress() {
+        toastValue(WalletCommunicationManager.getRpcAddress(COIN_TYPE))
+    }
+
+    fun toastValue(value : String) {
+        Toast.makeText(applicationContext, value, Toast.LENGTH_LONG).show()
     }
 
     // Catch result from send intent
